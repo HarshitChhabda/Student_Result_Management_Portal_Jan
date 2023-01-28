c5f3c19acd8892407544c303449174c4ee8e0808
Harshit Chhabda <harshit.chhabda@gmail.com>
2023-01-28 18:13:58 +0530

    Files Upload

---
+++ src/student/result/management_portal/StudentResultManagement_Portal.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package student.result.management_portal;
+
+
+public class StudentResultManagement_Portal {
+
+    
+    public static void main(String[] args) {
+        
+    }
+
+}
